8c6f5a7623da35c58f24c32f98dc19943557afd8
Anne Agda <anne.agda1@gmail.com>
2021-10-22 16:48:06 +1100

    deleted the health bar script and added the contents to the UI manager script instead

--- COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
+++ COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
@@ -8,6 +8,8 @@ public class UIManager : MonoBehaviour
 {
     public Text timerText;
     private float timer;
+    public Slider slider;
+
 
     void Update()
     {
@@ -17,11 +19,22 @@ public class UIManager : MonoBehaviour
         timerText.text = "Timer: " + FormatTime(timer); 
     }
 
+    //put this into its own script
     private string FormatTime(float time) 
     {
         TimeSpan timeSpan = TimeSpan.FromSeconds(time);
 
         return string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds);
     }
+        
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
     
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
 }
