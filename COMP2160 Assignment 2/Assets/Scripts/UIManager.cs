0a231a17f219516c0afe0d04f6a33b5dfd32aeb1
Anne Agda <anne.agda1@gmail.com>
2021-10-22 17:08:03 +1100

    Made the UI manager into a singleton

--- COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
+++ COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
@@ -6,9 +6,35 @@ using System;
 
 public class UIManager : MonoBehaviour
 {
+    // Singleton
+    static private UIManager instance;
+    static public UIManager Instance 
+    {
+        get 
+        {
+            if (instance == null) 
+            {
+                Debug.LogError("There is not UIManager in the scene.");
+            }            
+            return instance;
+        }
+    }
     public Text timerText;
     private float timer;
     public Slider slider;
+
+    void Awake() 
+    {
+        if (instance != null)
+        {
+            // if there is already a UI manager in the scene, then destroy this one
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
 
     void Update()
     {
