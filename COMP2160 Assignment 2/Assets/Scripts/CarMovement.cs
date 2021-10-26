2fac8d0350b3377fd5f8d03f22aea2b4032cbe6a
Julian-Baxter <69179532+Julian-Baxter@users.noreply.github.com>
2021-10-26 19:33:56 +1100

    Editing the Car Movement

--- COMP2160 Assignment 2/Assets/Scripts/CarMovement.cs
+++ COMP2160 Assignment 2/Assets/Scripts/CarMovement.cs
@@ -46,24 +46,10 @@ public class CarMovement : MonoBehaviour
     }
     void FixedUpdate()
     {
-        //Vector3 direction = Vec - this.transform.position;
-		//rb.velocity = rb.velocity + direction*Time.fixedDeltaTime;
-		//rb.AddForce(direction);
-    }
-    private Vector3 MousePosition()
-    {
-        // use raycasting to turn mouse position into position on the board
-        Plane plane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float t;
-        plane.Raycast(ray, out t);
-        return ray.GetPoint(t);
+        Vector3 direction = Vec - this.transform.position;
+		rb.velocity = rb.velocity + direction*Time.fixedDeltaTime;
+		rb.AddForce(direction);
     }
 
-	/*void FixedUpdate()
-    {
-        Vector3 direction = Vec - this.transform.position;
-        rb.velocity = rb.velocity + direction*Time.fixedDeltaTime;
-    }*/
 
 }
