8d10ac6aa7fd52bb58febce4891f9e240fa3497b
Julian-Baxter <69179532+Julian-Baxter@users.noreply.github.com>
2021-10-27 12:06:21 +1100

    Updating the Car Movement

--- COMP2160 Assignment 2/Assets/Scripts/CarMovement.cs
+++ COMP2160 Assignment 2/Assets/Scripts/CarMovement.cs
@@ -11,45 +11,103 @@ public class CarMovement : MonoBehaviour
     public float turningSpeed = 30;
     public float friction;
     public float gravity;
-    public float drag;
+    public float drag = 2;
 	private bool onGround = true;
 	private Rigidbody rb;
-	private Vector3 Vec;
+	private Vector3 velocity = Vector3.forward;
+    //private State state;
+	private float dx;
+	private float dy;
+	public BoxCollider groundCollider;
 
 
 
-	
 	// Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        //rb.isKinematic = true;
-		//rb.useGravity = true;
+		//state = State.OnGround;
+		rb = gameObject.GetComponent<Rigidbody>();
+		rb.useGravity = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dx = Input.GetAxis("Horizontal");
-        float dy = Input.GetAxis("Vertical");
-        //Vector3 position = MousePosition();
-        //Vec = position;
-		Vector3 velocity = new Vector3(0,0,20);
-		transform.Translate(velocity * Time.deltaTime*dy);
-        float angle = turningSpeed * Time.deltaTime;
-		
-		if(dy!=0)
+		if(onGround)
 		{
-		transform.Rotate(angle * Vector3.up*dx);
-
+			dx = Input.GetAxis("Horizontal");
+			dy = Input.GetAxis("Vertical");
+			if(dy!=0)
+			{
+				transform.Rotate(turningSpeed*Vector3.up*dx*Time.deltaTime);
+				rb.AddForce(accelerationSpeed*Vector3.forward*dy);
+				rb.AddForce(drag*Vector3.back*dy);
+			}
 		}
+		else
+		{
+			dx = 0;
+			dy = 0;
+		}
+		
+		
     }
-    void FixedUpdate()
+   /*void FixedUpdate()
     {
-        Vector3 direction = Vec - this.transform.position;
-		rb.velocity = rb.velocity + direction*Time.fixedDeltaTime;
-		rb.AddForce(direction);
-    }
-
+		if(Input.GetAxis("Vertical")!=0)
+		{
+		}
+		rb.velocity = rb.velocity + velocity*Time.fixedDeltaTime;
+		rb.AddForce(velocity);
+    }*/
+	void OnTriggerEnter(Collider other)
+	{
+		onGround = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		onGround = false;
+	}
+	
+		/*switch (state)
+        {
+            case State.OnGround:
+					dx = Input.GetAxis("Horizontal");
+					dy = Input.GetAxis("Vertical");
+					if(dy!=0)
+					{
+						transform.Rotate(turningSpeed*Vector3.up*dx*Time.deltaTime);
+					}
+					Debug.Log("State = Rise");
+					state = State.InAir;
+            break;
+            case State.InAir:
+					dx = Input.GetAxis("Horizontal");
+					dy = Input.GetAxis("Vertical");
+					if(dy!=0)
+					{
+						transform.Rotate(turningSpeed*Vector3.up*dx*Time.deltaTime);
+					}
+					Debug.Log("State = Rise");
+					state = State.UpSideDown;
+                  break;
+            case State.UpSideDown:
+                
+					dx = Input.GetAxis("Horizontal");
+					dy = Input.GetAxis("Vertical");
+					if(dy!=0)
+					{
+						transform.Rotate(turningSpeed*Vector3.up*dx*Time.deltaTime);
+					}
+					Debug.Log("State = Rise");
+					state = State.OnGround;
+                
+				break;
+		}*/	
+	/*private enum State {
+        OnGround,
+        InAir,
+		UpSideDown
+    }*/
 
 }
