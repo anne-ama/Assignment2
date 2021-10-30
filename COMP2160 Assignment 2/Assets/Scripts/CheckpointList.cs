faf8e7a307691fb9e2162b8bc14d36738b91f0c4
Anne Agda <anne.agda1@gmail.com>
2021-10-30 14:27:16 +1100

    fixed the checkpoint script to tune it to the requirements of the game better
    Where triggering the active checkpoint will deactivate that checkpoint and activate the next one

--- COMP2160 Assignment 2/Assets/Scripts/CheckpointList.cs
+++ COMP2160 Assignment 2/Assets/Scripts/CheckpointList.cs
@@ -7,14 +7,17 @@ public class CheckpointList : MonoBehaviour
     private List<CheckpointSingle> checkpointSingleList;
     private List<float> checkpointTimeList;
     public Transform checkpointList;
-    private int nextCheckpointSingleIndex;
+    private int currentIndex;
     private CheckpointSingle checkpointSingle;
 
     private void Awake() {
         //Adding checkpoints to the list to track 
         Transform checkpointsTransform = checkpointList;
         checkpointSingleList = new List<CheckpointSingle>();
+        //Create a new time list to record times 
         checkpointTimeList = new List<float>();
+
+        //Cycle through and add 
         foreach (Transform checkpointSingleTransform in checkpointsTransform)
         {
             Debug.Log(checkpointSingleTransform);
@@ -22,32 +25,45 @@ public class CheckpointList : MonoBehaviour
             checkpointSingle.SetCheckpointList(this);
             checkpointSingleList.Add(checkpointSingle);
         }
-        nextCheckpointSingleIndex = 0;
+        currentIndex = 0;
+    }
+
+    public void ActivateNextCheckpoint()
+    {
+        checkpointSingleList[currentIndex].isActive();
     }
 
     public void PlayerThroughCheckpoint(CheckpointSingle checkpointSingle)
     {
-        if (checkpointSingleList.IndexOf(checkpointSingle) == nextCheckpointSingleIndex)
+        //if the index of the one we are passing through is the correct index
+        if (checkpointSingleList.IndexOf(checkpointSingle) == currentIndex)
         {
             //Correct checkpoint passed through
             Debug.Log("Correct Checkpoint");
-            //this loops back to 0 however this may be unnecessary
-            // nextCheckpointSingleIndex = (nextCheckpointSingleIndex + 1) % checkpointSingleList.Count;
-            nextCheckpointSingleIndex++;
-            checkpointSingle.isActive();
+
+             //Log the time that it passes through the checkpoint
             checkpointTimeList.Add(Time.realtimeSinceStartup);
             Debug.Log(Time.realtimeSinceStartup);
 
-            if (nextCheckpointSingleIndex >= checkpointSingleList.Count)
+            //If this is the last checkpoint then indicate that it is the end of the game
+            if (currentIndex == checkpointSingleList.Count - 1)
             {
-                Debug.Log("last checkpoint");
+                // currentIndex = checkpointSingleList.Count;
+                Debug.Log("Game Won!");
             }
+            else {
+                currentIndex++;
+                ActivateNextCheckpoint();
+            }
+
         }
+
         else
         {
             checkpointSingle.notActive();
-            //wrong checkpoint passed through
             Debug.Log("Wrong Checkpoint");
         }
     }
+
+
 }
